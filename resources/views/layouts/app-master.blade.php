a96125fd6cc39e1c161ac4c2dcaace7e1748662e
Mark Remetio <mark.remetio@razzaconsulting.com>
2024-01-23 08:28:11 +0800

    Admin UI Revisions and Updates

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-    <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -11,46 +12,45 @@
     <!-- Bootstrap core CSS -->
     <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .float-right {
-        float: right; 
-      }
-
-    </style>
-
     <style id="antiClickjack">
-        body{display:none !important;}
+        body {
+            display: none !important;
+        }
     </style>
 
     <!-- Custom styles for this template -->
-    <link href="{!! url('assets/css/app.css') !!}" rel="stylesheet">
+    <link href="{!! url('assets/css/admin.css') !!}" rel="stylesheet">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500&display=swap" rel="stylesheet">
 
 </head>
+
 <body>
 
-    @include('layouts.partials.navbar')
 
-    <main class="container mt-5">
-        @yield('content')
-    </main>
+    <div class="mt-5">
+        @include('layouts.partials.admin-sidebar')
+    </div>
 
-    <script type="text/javascript"> 
+    <div id="content" class="page-content-wrapper">
+        <main class="container-fluid">
+            @include('layouts.partials.banner')
+
+            @yield('content')
+        </main>
+        <div class="footer">
+            @include('auth.partials.footer')
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="{!! url('assets/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript">
         if (self === top) {
             var antiClickjack = document.getElementById("antiClickjack");
             antiClickjack.parentNode.removeChild(antiClickjack);
@@ -59,11 +59,44 @@
         }
     </script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="{!! url('assets/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
+    <script>
+        var isOpen = true;
 
-    @section("scripts")
+        function toggleNav() {
+            if (isOpen) {
+                closeNav();
+            } else {
+                openNav();
+            }
+        }
+
+        function openNav() {
+            document.getElementById("adminSidebar").style.width = "300px";
+            document.getElementById("content").style.marginLeft = "300px";
+            document.getElementById("toggleButton").innerHTML = '<i class="fas fa-arrow-left"></i>'; // Right arrow icon
+            isOpen = true;
+        }
+
+        function closeNav() {
+            document.getElementById("adminSidebar").style.width = "0";
+            document.getElementById("content").style.marginLeft = "0";
+            document.getElementById("toggleButton").innerHTML = '<i class="fas fa-arrow-right"></i>'; // Left arrow icon
+            isOpen = false;
+        }
+
+        document.getElementById("toggleButton").innerHTML = '<i class="fas fa-arrow-left"></i>';
+
+        document.getElementById("toggleButton").onclick = toggleNav;
+        $(function() {
+            $('[data-bs-toggle="tooltip"]').tooltip()
+        })
+    </script>
+
+   
+
+    @section('scripts')
 
     @show
-  </body>
+</body>
+
 </html>
---
+++ resources/views/layouts/partials/admin-sidebar.blade.php
@@ -0,0 +1,77 @@
+<div id="adminSidebar" class="sidenav bg-light">
+    <ul class="list-unstyled components">
+        <!-- User Management -->
+        <li class="nav-item">
+            <a href="#userManagementSubmenu" data-bs-toggle="collapse" aria-expanded="true" class="dropdown-toggle rounded fw-bold">
+                <i class="fas fa-users-cog"></i> User Management
+            </a>
+            <ul class="collapse list-unstyled" id="userManagementSubmenu">
+                <li><a href="{{ route('users.index') }}"><i class="fas fa-user"></i> Users</a></li>
+                <li><a href="{{ route('roles.index') }}"><i class="fas fa-user-tag"></i> Roles</a></li>
+                <li><a href="{{ route('permissions.index') }}"><i class="fas fa-key"></i> Permissions</a></li>
+            </ul>
+        </li>
+
+        <!-- Employee Management -->
+        <li class="nav-item">
+            <a href="#employeeManagementSubmenu" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle rounded fw-bold">
+                <i class="fas fa-briefcase"></i> Employee Management
+            </a>
+            <ul class="collapse list-unstyled" id="employeeManagementSubmenu">
+                <li><a href="{{ route('employees.index') }}"><i class="fas fa-id-badge"></i> Employees</a></li>
+                <li><a href="{{ route('functionalgroups.index') }}"><i class="fas fa-layer-group"></i> Functional Groups</a></li>
+                <li><a href="{{ route('bureaus_offices.index') }}"><i class="fas fa-building"></i> Bureaus / Offices</a></li>
+                <li><a href="{{ route('divisions.index') }}"><i class="fas fa-code-branch"></i> Division</a></li>
+                <li><a href="{{ route('positions.index') }}"><i class="fas fa-user-md"></i> Position</a></li>
+            </ul>
+        </li>
+
+        <!-- Competency Management -->
+        <li class="nav-item">
+            <a href="#competencyManagementSubmenu" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle rounded fw-bold">
+                <i class="fas fa-chart-line"></i> Competency Management
+            </a>
+            <ul class="collapse list-unstyled" id="competencyManagementSubmenu">
+                <li><a href="{{ route('competency_categories.index') }}"><i class="fas fa-th-list"></i> Competency Categories</a></li>
+                <li><a href="{{ route('competencies.index') }}"><i class="fas fa-tasks"></i> Competencies</a></li>
+                <li><a href="{{ route('behavioral_indicators.index') }}"><i class="fas fa-tasks"></i> Behavioral Indicators</a></li>
+                <li><a href="{{ route('competency_sets.index') }}"><i class="fas fa-boxes"></i> Competency Sets</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top shadow-lg rounded">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{!! url('/images/dti-logo.ico') !!}" alt="" width="32" height="32">
+        </a>
+        <div class="d-sm-none d-md-block d-none d-sm-block">
+            <span id="toggleButton"
+                class="text-white rounded-circle d-inline-flex justify-content-center align-items-center "
+                onclick="toggleNav()" style="cursor: pointer;">
+                <i class="fas fa-arrow-left"></i>
+            </span>
+        </div>
+
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-black"
+                        href="{{ route('profile.index', ['user' => auth()->user()->id]) }}">
+                        {{ auth()->user()->firstname }} {{ auth()->user()->lastname }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout.perform') }}">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
