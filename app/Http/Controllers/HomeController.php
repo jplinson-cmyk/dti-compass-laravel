d57fe5ba9af25e0fabc946ea1cde07f080d136d8
Mark Remetio <mark.remetio@razzaconsulting.com>
2024-01-30 10:27:32 +0800

    Index Page fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,8 +25,6 @@ class HomeController extends Controller
     public function index(){
 
 
-        $url = route("password.reset","asdfasdgadsgasdgasghasdhadshash");
-        dd($url);
         $categories = CompetencyCategory::all();
         $user = auth()->user();
 
